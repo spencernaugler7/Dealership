9ebdd551c4c68049de6bd6152e97f37af3295908
TheLuckyRogue <spencernaugler7@gmail.com>
2019-04-12 13:36:45 -0400

    added Menu class for main menu.

---
+++ src/Menu.java
@@ -0,0 +1,10 @@
+public class Menu {
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static String menu(){
+        String choice = "";
+        return choice;
+    }
+}
